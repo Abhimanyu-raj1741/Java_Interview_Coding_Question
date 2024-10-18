277dca81534ed8b07cdf5fa9c9084fc162b9b241
Abhimanyu <abhimanyu.raj1741@gmail.com>
2024-10-19 00:51:33 +0530

    java 8 code

--- CodingQuestion/src/streamAPI/MultipleOf5.java
+++ CodingQuestion/src/streamAPI/MultipleOf5.java
@@ -1,5 +1,15 @@
 package streamAPI;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class MultipleOf5 {
 
+	public static void main(String[] args) {
+		
+		List<Integer> ls = Arrays.asList(20, 23 , 34 ,5, 10);
+		
+		ls.stream().filter(num->num%5==0).forEach(System.out::println);
+		
+	}
 }
